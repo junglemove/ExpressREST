deca38010874c38b088fc2fc82aa1a7c2d2504a1
junglemove <thibaut.visticot@gmail.com>
2016-12-14 15:35:55 +0100

    Exo init

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,6 +5,23 @@ const Router = require('express').Router;
 
 let router = new Router();
 
+const contacts = [{
+    prenom:'Bill',
+    nom:'Gates',
+    id:123
+},{
+    prenom:'Steve',
+    nom:'Jobs',
+    id:321
+}]
+
+// 1 - Creer la vue liste qui fait le rendu du tableau
+// (regader la docs de http://ejs.co)
+
+
+// 2 - Créer la vue afficher qui affiche le contact reçu
+// dans l'url (req.params.id)
+
 router.get('/', (req, res, next) => {
     res.send('<h2>Liste des contacts</h2>');
 });
