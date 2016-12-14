92186940232fddc2785249bd1bcecf792a86e1db
junglemove <thibaut.visticot@gmail.com>
2016-12-14 16:55:08 +0100

    Exo use of form post req.body

--- routes/contacts.js
+++ routes/contacts.js
@@ -22,8 +22,28 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
     var id = req.params.id;
     Contact.findById(id, (err, contacts) =>{
+
+        if(err){
+            return next(err);
+        }
+
+        if(!contacts){
+            req.message = "Le contact n'existe pas";
+            return next();
+        }
+
         res.render('contact/show', {contacts}); // since ES6 {contacts} can replace {contacts:contacts}
     });
+});
+
+router.get('/add', (req, res, next) =>{
+    res.render('contact/add');
+});
+
+router.post('/add', (req, res, next) =>{
+    console.log(req.body);
+    //contacts.push({})
+    res.redirect('/');
 });
 
 module.exports = router;
