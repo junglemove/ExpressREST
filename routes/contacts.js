4c24e2ee538c71d4010f2b1ffbe4ade6aba1ece9
junglemove <thibaut.visticot@gmail.com>
2016-12-14 16:23:35 +0100

    Exo 1

--- routes/contacts.js
+++ routes/contacts.js
@@ -17,18 +17,18 @@ const contacts = [{
 
 // 1 - Creer la vue liste qui fait le rendu du tableau
 // (regader la docs de http://ejs.co)
+router.get('/', (req, res, next) => {
+    res.render('contacts', {users:contacts});
+});
+
 
 // 2 - Créer la vue afficher qui affiche le contact reçu
 // dans l'url (req.params.id)
-
-router.get('/', (req, res, next) => {
-
-    res.render('contacts', {contacts});
-    //res.send('<h2>Liste des contacts</h2>');
-});
-
 router.get('/:id', (req, res, next) => {
-    res.send(`<h2>Liste des contacts ${req.params.id}</h2>`);
+    var id = req.params.id;
+    var oneContact = contacts.find((contact) =>   contact.id == id );
+    console.log("Found: "+ id + "/" + oneContact);
+    res.render('contacts', {users:oneContact});
 });
 
 module.exports = router;
