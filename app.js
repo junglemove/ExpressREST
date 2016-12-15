e32fe381c983598e2343aeb5c5997c844d2ebff9
junglemove <thibaut.visticot@gmail.com>
2016-12-15 09:48:57 +0100

    JSON middleware

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Filter that parse request data to json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); // Allows to serve css and all (CORS support)
